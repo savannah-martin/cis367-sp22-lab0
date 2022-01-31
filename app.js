a0cbee1b384faf60b782cd3ab7c804a54a1d4bff
Savannah Martin <78399457+savannah-martin@users.noreply.github.com>
2022-01-31 01:55:14 -0600

    done?

--- app.js
+++ app.js
@@ -5,26 +5,46 @@ const hostname = "localhost";
 const port = 3000;
 
 app.get('/', (req, res)=> {
-    res.send("Hey, it works!");
+    res.send("Welcome to Lab 0");
 });
 
 // Query String Parameters
-// /pokemon?name=pikachu
-app.get('/pokemon', (req, res)=> {
+// /greet?name=Sean&year=2002
+app.get('/greet', (req, res)=> {
 
-    let name = req.query.name.toUpperCase();
-    res.send(`<h1> ${name} </h1>`);
-
+    let name = req.query.name;
+    let year = parseInt(req.query.year);
+    res.send(`<h1> Hello, ${name}! You are ${2021 - year} or ${2022 -year} years old.</h1>`);
 });
 
 // Route Parameters
-// /pokemon/pikachu
-app.get('/timesten/:id', (req, res)=> {
+// /math/6/times/7
+app.get('/math/:num1/:math/:num2', (req, res)=> {
 
-    let id = parseInt(req.params.id);
-    console.log(id);
+    let num1 = parseInt(req.params.num1);
+    console.log(num1);
+    let num2 = parseInt(req.params.num2);
+    console.log(num2);
 
-    res.send(`<h1> ${id * 10} </h1>`);
+    let math = req.params.math;
+
+    console.log(math);
+    switch(math) {
+        case "plus":
+            res.send(`<h1> ${num1 + num2} </h1>`);
+          break;
+        case "minus":
+            res.send(`<h1> ${num1 - num2} </h1>`);
+          break;
+        case "times":
+            res.send(`<h1> ${num1 * num2} </h1>`);
+          break;
+        case "dividedby":
+            res.send(`<h1> ${num1 / num2} </h1>`);
+          break;
+        default:
+            res.send(`<h1> Please use a different wordle. </h1>`);
+      }
 
 });
 
